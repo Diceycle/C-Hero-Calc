b9f32652fb60da7dc41f85d6fd17bddf0701dd02
Diceycle <olafmeins@freenet.de>
2017-08-10 21:42:42 +0200

    Add prototype of monster comparison

--- main.cpp
+++ main.cpp
@@ -39,6 +39,12 @@ bool isCheaper(Monster * a, Monster * b) {
     return a->cost < b->cost;
 }
 
+// Function determining if a monster is strictly better than another
+bool isBetter(Monster * a, Monster * b, bool considerAbilities = false) {
+    if (a->element == b->element) {
+        return (a->damage >= b->damage) && (a->hp >= b->hp);
+    } else { // a needs to be better than b even when b has elemental advantage, or a is at disadvantage
+        return !considerAbilities && (a->damage >= b->damage * elementalBoost) && (a->hp >= b->hp * elementalBoost);
     }
 }
 
