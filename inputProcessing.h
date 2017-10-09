5619d6af0d5728b73443e7232babcbf15e18e722
Diceycle <olafmeins@freenet.de>
2017-10-09 19:21:50 +0200

    Update Help entries

--- inputProcessing.h
+++ inputProcessing.h
@@ -25,10 +25,6 @@ struct Instance {
     size_t maxCombatants;
 };
 
-const string inputModeQuestion = "Do you want to input everything via command line?";
-const string inputModeHelp =
-    "  If YES is selected all necessary data will be asked of you here.\n" 
-    "  Note that selecting NO only makes sense if you have edited the data in the script and compiled the program for yourself! \n";
 const string heroFileNotFoundErrorMessage = 
     "  Could not find Hero File or Hero File is deprecated. (Were there any new Heroes added recently?)\n"
     "  Switching to manual input mode...\n";
@@ -42,15 +38,15 @@ const string heroInputHelp =
     "  Enter the Level of the hero whose name is displayed on the left. If you don't own that hero, enter 0.\n";
 
 const string lineupInputHelp = 
-    "  Enter Monsters separated by a comma and no spaces.\n"
+    "  Enter Monsters separated by commas.\n"
     "  Normal monsters are written with their element (a,e,w,f) and their tier number. So the level 5 water monster is w5.\n"
     "  Heroes are written first with their full name and a colon(:) followed by their level. For example: forestdruid:50\n"
     "  Full example: a1,geror:22,f13,w2,ladyoftwilight:1\n"
-    "  The other alternative is selecting a quest from the game. For example: Typing quest23 loads the lineup for the 23rd quest.\n";
-    
-const string maxMonstersAllowedHelp =
-    "  This determines the maximum size of your lineup.\n"
-    "  The most common input here is 6 with the exception of doing quests where it can also be 5 or 4.\n";
+    "  The other alternative is selecting a quest from the game. "
+    "For example: Typing quest23-3 loads the lineup for the 23rd quest and tries to beat it with 4 monsters or less.\n"
+    "  You can also enter multiple lineups at once. Do so by separating them with spaces.\n"
+    "  Example: quest5-3 quest6-3 quest7-2 a1,a2,a3 quest8-1\n"
+    "  In this Example the program will calculate those five lineups one after another.\n";
     
 const string minimumMonsterCostHelp = 
     "  This determines how expensive a monster needs to be in order for the calculator to consider it for a solution.\n"
