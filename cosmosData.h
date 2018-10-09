671af4d9bdeb89404e4bdd9522ee67b7c99a147b
Martin <41296343+GizmoMar@users.noreply.github.com>
2018-10-09 19:26:39 +0200

    3.0.9.1f
    Changed them all to universal format of LF

--- cosmosData.h
+++ cosmosData.h
@@ -14,7 +14,7 @@
 #include <fstream>
 
 // Version number not used anywhere except in output to know immediately which version the user is running
-const std::string VERSION = "3.0.9.1e";
+const std::string VERSION = "3.0.9.1f";
 
 const size_t GIGABYTE = ((size_t) (1) << 30);
 
