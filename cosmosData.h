104a913483ea9e5d289746c176aa9d1b0f2d4aee
Martin <41296343+GizmoMar@users.noreply.github.com>
2018-11-26 19:39:33 +0200

    3.2.0.4d
    Thanks Mr Z

--- cosmosData.h
+++ cosmosData.h
@@ -14,7 +14,7 @@
 #include <fstream>
 
 // Version number not used anywhere except in output to know immediately which version the user is running
-const std::string VERSION = "3.2.0.4a";
+const std::string VERSION = "3.2.0.4d";
 
 const size_t GIGABYTE = ((size_t) (1) << 30);
 
